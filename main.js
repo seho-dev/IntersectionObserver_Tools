cfcf8ef418e099ab80a94a65edd2240ae8b15707
1018715564 <1018715564@qq.com>
2019-11-17 22:11:02 +0800

    初始化包&添加懒加载基础功能&触底加载基础功能

---
+++ main.js
@@ -0,0 +1,72 @@
+/**
+ * 
+ * 入口js函数
+ * 
+ */
+
+
+const LAZY_LOAD_ATTRIBUTE = "data-observerLazyLoad";
+
+/**
+ * 
+ * @param {*} $type 判断使用哪种工具
+ * @param {*} callback 监听回调
+ * @param {*} target 监听目标元素
+ * @param {*} $option 自定义配置参数
+ */
+function observerTools($type = "lazyLoad", target, $option, callback) {
+    // 创建触底对象
+    let onBottom = null;
+
+    // 创建observer对象
+    let observe = new IntersectionObserver(function (entries) {
+        // 判断type传参
+        switch ($type) {
+            case "lazyLoad":
+                // 判断元素和指定视口是否发生交叉，如果发生交叉
+                entries.forEach(function (entriesChild, index) {
+                    if (entriesChild.isIntersecting) {
+                        // 取出监听元素的自定义属性
+                        entriesChild.target.src = entriesChild.target.getAttribute(LAZY_LOAD_ATTRIBUTE);
+                        // 移除监听的元素
+                        observe.unobserve(entriesChild.target);
+                    }
+                })
+                break;
+            case "onBottom":
+                // 如果末尾元素进入视口，则触底
+                if (entries[0].isIntersecting) {
+                  return callback(entries[0]);
+                }
+                break;
+        }
+    }, $option);
+    // 如果type是onBottom
+    switch ($type) {
+        case "lazyLoad":
+            // 判断目标元素是否存在, 如果是元素数组，就循环监听
+            target.forEach(function (element) {
+                let checkResult = isElement(element);
+                if (checkResult) {
+                    observe.observe(element); // 监听元素
+                }
+            });
+            break;
+        case "onBottom":
+            // 在末尾追加一个标识符
+            onBottom = document.createElement("div");
+            // 给目标元素末尾添加标识符div
+            onBottom.classList.add("observe_bottom_main")
+            target[0].appendChild(onBottom);
+            observe.observe(onBottom)
+            break;
+    }
+
+}
+
+// 判断目标元素是否是element，不同浏览器类型都不一样
+function isElement(obj) {
+    return (typeof HTMLElement === 'object') ?
+        (obj instanceof HTMLElement) :
+        !!(obj && typeof obj === 'object' && (obj.nodeType === 1 || obj.nodeType === 9) && typeof obj.nodeName === 'string');
+}
